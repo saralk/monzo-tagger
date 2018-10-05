d73698fa3d1f6862c31cf2f265007765f23a26bb
Saral Kaushik <saral.kaushik@vincuventas.com>
2018-10-05 22:20:16 +0100

    add notes

--- clients/monzo.js
+++ clients/monzo.js
@@ -24,6 +24,16 @@ class Monzo {
         }, cb);
     }
 
+    makePatchRequest(endpoint, params, cb) {
+        params.account_id = this.account_id;
+        const url = `${MONZO_API_DOMAIN}/${endpoint}?${this.convertObjectToUrlParams(params)}`; 
+        return request.patch(url, {
+            'auth': {
+                'bearer': this.access_token
+            }
+        }, cb);
+    }
+
     getTransactions(cb) {
         this.makeGetRequest('transactions', {'expand[]':'merchant'}, (err, res, body) => {
             if (err) {
@@ -33,6 +43,10 @@ class Monzo {
 
             cb(false, JSON.parse(body).transactions);
         });
+    }
+
+    setNotes(notes) {
+        this.makePatchRequest('transactions', {'metadata[notes]':notes});
     }
 }
 
